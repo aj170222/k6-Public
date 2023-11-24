578fe0e97dcb05d02f4d8de6df2543075cd9539e
aj170222 <139350694+aj170222@users.noreply.github.com>
2023-11-24 11:49:43 +0530

    Update complete_script.js

--- Epic_EHR/Scripts/complete_script.js
+++ Epic_EHR/Scripts/complete_script.js
@@ -4,8 +4,8 @@ import {check} from 'k6';
 //import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
-    vus: 5,
-    duration: "2s",
+    vus: 1000,
+    duration: "10m",
     //iterations: 50,
   };
 
