8df67f8c40fecf9827aa1a82e185b755dd96162e
Ashwini Joshi <ashwini.joshi@nitorinfotech.com>
2023-11-24 15:55:34 +0530

    new

--- Epic_EHR/Scripts/single_request_complete_script.js
+++ Epic_EHR/Scripts/single_request_complete_script.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import {check} from 'k6';
 
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+//import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     vus: 1000,
@@ -30,11 +30,10 @@ export default function () {
 }
 
 
+// export function handleSummary(data) {
+//   console.log("creating html report*****************")
+//   return {
+//     "single_requests_complete_report.html": htmlReport(data),
+//   };
 
-export function handleSummary(data) {
-  console.log("creating html report*****************")
-  return {
-    "single_requests_complete_report.html": htmlReport(data),
-  };
-
-}
+// }
